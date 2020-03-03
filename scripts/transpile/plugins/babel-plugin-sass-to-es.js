27c08857a1c42443330dd09c4357f53a6661655f
mykas <mykolass@wix.com>
2020-03-03 12:58:07 +0200

    Add basic unit tests for babel plugins

--- scripts/transpile/plugins/babel-plugin-sass-to-es.js
+++ scripts/transpile/plugins/babel-plugin-sass-to-es.js
@@ -3,43 +3,42 @@ const path = require('path');
 // This plugin transpiles sass imports to imports from dist/es
 // this solves the problem for ssr where we have classname mismatch.
 
+function transpileSASS(api, state) {
+  const originalPath = api.node.source.value;
+  if (/[A-Za-z]*\.scss/.test(originalPath)) {
+    const { file } = state;
+
+    // we check which files tries to import the sass file
+    const ComponentFile = file.opts.generatorOpts.sourceFileName;
+
+    // then we can get absolute path of sass file
+    const absolutePathSass = path.resolve(
+      file.opts.filename.replace(ComponentFile, ''),
+      originalPath,
+    );
+
+    // we construct sass import to be from es folder
+    const sassES = absolutePathSass.replace('src/', 'dist/es/src/');
+
+    // we construct the component that tries to import sass file to be in
+    // common js path
+    const fileCJS = file.opts.filename
+      .replace(ComponentFile, '')
+      .replace('src/', 'dist/src/');
+
+    // we get a relative path for component to import from es
+    const relative = path.relative(fileCJS, sassES);
+
+    api.node.source.value = relative;
+  }
+}
+
 module.exports = function() {
   return {
     name: 'sass-to-es',
     visitor: {
       ImportDeclaration(api, state) {
-        const originalPath = api.node.source.value;
-
-        if (/[A-Za-z]*\.scss/.test(originalPath)) {
-          const { file } = state;
-
-          // we check which files tries to import the sass file
-          const ComponentFile = file.opts.generatorOpts.sourceFileName;
-
-          // then we can get absolute path of sass file
-          const absolutePathSass = path.resolve(
-            file.opts.filename.replace(ComponentFile, ''),
-            originalPath,
-          );
-
-          // we construct sass import to be from es folder
-          const sassES = absolutePathSass.replace('src/', 'dist/es/src/');
-
-          // we construct the component that tries to import sass file to be in
-          // common js path
-          const fileCJS = file.opts.filename
-            .replace(ComponentFile, '')
-            .replace('src/', 'dist/src/');
-
-          // we get a relative path for component to import from es
-          const relative = path.relative(fileCJS, sassES);
-
-          console.log('sassEs', sassES); // eslint-disable-line
-          console.log('fileCJS', fileCJS); // eslint-disable-line
-          console.log('relative', relative); // eslint-disable-line
-
-          api.node.source.value = relative;
-        }
+        transpileSASS(api, state);
       },
     },
   };
